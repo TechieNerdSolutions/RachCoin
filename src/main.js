46525bfa1acdf11e9fe181f8ee185b23e592770d
Rachamv <rkutalian@gmail.com>
2024-04-13 12:27:53 +0100

    delete

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'counterup/jquery.counterup.min.js';
-import 'easing-js/easing.js';
-import 'owl.carousel/dist/owl.carousel.min.js';
-import 'wowjs/dist/wow.min.js';
